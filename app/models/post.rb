4338bcf95d96eb3982c06c7fb864f388f1d1fd03
Felice Forby <feliceforby@gmail.com>
2018-10-08 22:56:06 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, inclusion: { in: w%(Fiction Non-Fiction), message: "must be Fiction or Non-Fiction"}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "must be Fiction or Non-Fiction"}
 end
